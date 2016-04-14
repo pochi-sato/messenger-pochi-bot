852c30def51744e1d9655d3b8844dab22330b9c8
Takuto Sato <takuto@Tacknyan.local>
2016-04-15 00:57:44 +0900

    メッセージを受け取った際のオウム返しを実装

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,28 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    request = require('request'),
+    router = express.Router(),
+    token = "<validation_token>";
+
+function sendTextMessage(sender, text) {
+  var messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
 
 router.get('/', function(req, res, next) {
   if (req.query['hub.verify_token'] === '<validation_token>') {
@@ -8,6 +31,19 @@ router.get('/', function(req, res, next) {
   res.send('Error, wrong validation token');
 });
 
+router.post('/', function(req, res, next) {
+  var messaging_events = req.body.entry[0].messaging,
+      replayMessages = [];
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+
+      sendTextMessage(sender, text.substring(0, 180) + "だにゃ〜");
+    }
+  }
+  res.sendStatus(200);
 });
 
 module.exports = router;
