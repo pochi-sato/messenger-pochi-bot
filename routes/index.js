8cb19c5e2fe3976bbf47178aaef264d4a2202100
Takuto Sato <takuto@Tacknyan.local>
2016-04-15 00:56:16 +0900

    URLチェック用GETメソッド実装

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if (req.query['hub.verify_token'] === '<validation_token>') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+});
+
 });
 
 module.exports = router;
